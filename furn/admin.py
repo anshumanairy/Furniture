fd4f5b6bd3f0b848133e43e3c35a6be7f7e8865f
Abhishek <abhishek73knp@gmail.com>
2020-05-03 11:01:04 +0530

    multiple images

--- furn/admin.py
+++ furn/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from furn.models.register_model import user_detail
+from furn.models.image_model import Image
 from furn.models.product_model import product_upload
 from django.contrib.auth.models import Permission
 from django.contrib import admin
@@ -7,3 +8,5 @@ from django.contrib import admin
 admin.site.register(Permission)
 admin.site.register(user_detail)
 admin.site.register(product_upload)
+admin.site.register(Image)
+
--- furn/controller/product.py
+++ furn/controller/product.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import login,authenticate,logout,get_user_model
 from django.contrib.auth.models import User
@@ -20,7 +20,19 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.models import Permission
 from django.utils.timezone import utc
 from django.utils import timezone
+from furn.models.product_model import product_upload
 
 @login_required(login_url='/')
 def prod(request):
     return render(request,'product.html/',{})
+
+def prod_detail(request,procode):
+    obj = get_object_or_404(product_upload,product_code=procode)
+    img = obj.get_images
+    context = {
+        'product' : obj,
+        'images' : img,
+    }
+    print("##debug")
+    print(obj,img)
+    return render(request,'product.html/',context)
